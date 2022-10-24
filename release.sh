80bc5a26b0d759bf50506f1496cc6070c88e1dd4
Rob Van Mieghem <robvanmieghem@gmail.com>
2022-10-24 13:41:37 +0200

    tar.gz the release binary

--- release.sh
+++ release.sh
@@ -2,3 +2,4 @@
 
 mkdir -p dist
 GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.Version=$(git describe)'" -o dist/
+cd dist && tar -czf libp2p-relay_$(git describe | sed 's/^v//')_linux_amd64.tar.gz libp2p-relay
